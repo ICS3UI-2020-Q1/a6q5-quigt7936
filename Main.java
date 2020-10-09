28fed5ed3872b3bd9cee8a7a3ae06e23e10d37c9
quigt7936 <71140817+quigt7936@users.noreply.github.com>
2020-10-09 08:35:15 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The program organizes a 2 index array in ascending order
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    // declare the array
+    int[] userNumbers = new int[2];
+
+    // get the two variables to put in the array
+    System.out.println("Please enter the 2 values for the array");
+    userNumbers[0] = input.nextInt();
+    userNumbers[1]  = input.nextInt();
+
+    // swap the smallest to 0
+    int swapNumber;
+    if (userNumbers[1] < userNumbers[0]) {
+      // this just makes a circle with the values, swap takes index 1, index 1 takes index 0, index 0 takes swap. 
+      swapNumber = userNumbers[1];
+      userNumbers[1] = userNumbers[0];
+      userNumbers[0] = swapNumber;
+    }
+
+    // tell the user the new order of the array
+    System.out.println("Your numbers in ascending order are " + userNumbers[0] + " and " + userNumbers[1]);
     
   }
 }
